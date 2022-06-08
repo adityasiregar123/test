b0e30a763917258d5840f5cf395b91245a143b6e
adityasiregar123 <studentclass335428@gmail.com>
2022-06-08 13:33:53 +0700

    Help me Lord

---
+++ src/Component/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Login = () => {
+  return (
+    <div style={{ backgroundColor: 'red '}}>
+      <Container>
+        <h2>Ngising gaming</h2>
+      </Container>
+    </div>
+  )
+}
+
+export default Login;
